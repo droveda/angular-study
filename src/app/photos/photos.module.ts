f6b9b419d077666c910b82ba1cfc9665b8984775
Diegues Roveda <dieguesroveda@gmail.com>
2021-05-02 18:48:44 -0300

    initial commit

---
+++ src/app/photos/photos.module.ts
@@ -0,0 +1,17 @@
+import { HttpClientModule } from "@angular/common/http";
+import { CommonModule } from "@angular/common";
+import { NgModule } from "@angular/core";
+import { PhotoComponent } from "./photo/photo.component";
+import { PhotoListComponent } from './photo-list/photo-list.component';
+import { PhotoFormComponent } from './photo-form/photo-form.component';
+import { PhotosComponent } from './photo-list/photos/photos.component';
+import { FilterByDescription } from "./photo-list/filter-by-description.pipe";
+
+
+@NgModule({
+    declarations: [ PhotoComponent, PhotoListComponent, PhotoFormComponent, PhotosComponent, FilterByDescription ],
+    imports: [ HttpClientModule, CommonModule ]
+})
+export class PhotosModule {
+
+}
